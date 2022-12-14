7c80003d3334a3ca2b129b0bb3b1b7bd037387a4
johannes <johannes.birk11@web.de>
2022-12-14 13:45:45 +0100

    make more columns of venue required

--- model.py
+++ model.py
@@ -9,10 +9,10 @@ class Venue(db.Model):
     __tablename__ = "Venue"
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    city = db.Column(db.String(120))
-    state = db.Column(db.String(120))
-    address = db.Column(db.String(120))
+    name = db.Column(db.String, nullable=False)
+    city = db.Column(db.String(120), nullable=False)
+    state = db.Column(db.String(120), nullable=False)
+    address = db.Column(db.String(120), nullable=False)
     phone = db.Column(db.String(120))
     genres = db.Column(db.ARRAY(db.String), nullable=False)
     image_link = db.Column(db.String(500))
